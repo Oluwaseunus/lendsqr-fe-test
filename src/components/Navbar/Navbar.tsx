5421f5cb2af33d69fd12d75fab4a541fc479ea0d
Oluwaseun <oluwaseunus@gmail.com>
2023-12-16 15:35:19 +0100

    feat(navbar.tsx): add profile and docs sections to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import Button from "../Button";
 import logo from "@/assets/images/logo.svg";
 import search from "@/assets/images/search.svg";
+import profile from "@/assets/images/profile.png";
+import dropdown from "@/assets/images/dropdown.svg";
+import notification from "@/assets/images/notification.svg";
 
 import styles from "./Navbar.module.scss";
 import { workSans } from "@/app/fonts";
@@ -24,6 +28,26 @@ export default function NavBar() {
         <Button variant="primary">
           <Image src={search} width={14} alt="Search" />
         </Button>
+      </div>
+
+      <div className={styles.actions}>
+        <Link href="#">Docs</Link>
+
+        <Image src={notification} alt="Notifications" width={26} />
+
+        <div className={styles.profile}>
+          <Image
+            width={48}
+            height={48}
+            src={profile}
+            alt="Adedeji"
+            objectFit="cover"
+          />
+
+          <p>Adedeji</p>
+
+          <Image src={dropdown} alt="More" width={20} />
+        </div>
       </div>
     </nav>
   );
--- src/pages/dashboard/users/users.tsx
+++ src/pages/dashboard/users/users.tsx
@@ -1,4 +1,3 @@
-import { uuid } from "uuidv4";
 import Image from "next/image";
 import { ReactElement, useMemo } from "react";
 
